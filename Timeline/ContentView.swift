38056544470e950622174469e6bf49ae276958be
miical <879754743@qq.com>
2022-12-27 10:19:05 +0800

    Initial Commit

---
+++ Timeline/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timeline
+//
+//  Created by Must77 on 2022/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timeline/TimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineApp.swift
+//  Timeline
+//
+//  Created by Must77 on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct TimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
