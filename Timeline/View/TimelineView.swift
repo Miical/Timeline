bb7358f8a0a2d8704469d9574dd9360c60870d00
miical <879754743@qq.com>
2022-12-27 21:03:11 +0800

    添加任务进行时的时间显示

--- Timeline/View/TimelineView.swift
+++ Timeline/View/TimelineView.swift
@@ -33,6 +33,7 @@ struct TimelineView: View {
                     }
                 }
             }
+            Spacer()
         }
     }
     
--- Timeline/View/TimingView.swift
+++ Timeline/View/TimingView.swift
@@ -9,19 +9,34 @@ import SwiftUI
 
 // 计时界面
 struct TimingView: View {
-    typealias OngoingTask = TimelineManager.OngoingTask
     @EnvironmentObject var timeline: Timeline
     
+    @State var timer: Timer?
+    @State var timeCostInSeconds: Int = 0
     var body: some View {
         VStack {
             Spacer()
+            let timeText = String(format: "%02d:%02d:%02d",
+                   timeCostInSeconds / 3600,
+                   timeCostInSeconds % 3600 / 60,
+                   timeCostInSeconds % 60)
+            Text(timeText)
+                .font(.largeTitle)
+                .fontWeight(.medium)
+            Spacer()
             if timeline.ongoingTask == nil {
                 Button("Start") {
+                    timeCostInSeconds = 0
                     timeline.startATask(of: timeline.taskCategoryList[0], at: Date())
+                    timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                        timeCostInSeconds = Calendar.current.dateComponents([.second],
+                            from: timeline.ongoingTask!.1, to: Date()).second!
+                    }
                 }
             } else {
                 Button("End") {
                     timeline.endTask(at: Date())
+                    timer?.invalidate()
                 }
             }
         }
