1b49756ce2c36736c27038755dc3e292ff14fe6f
miical <879754743@qq.com>
2022-12-31 21:59:25 +0800

    添加计划任务显示

--- Timeline/View/TimelineView.swift
+++ Timeline/View/TimelineView.swift
@@ -65,8 +65,7 @@ struct TimelineView: View {
                                 color: timeline.taskCategory(id: completedTask.taskCategoryId).color,
                                 time: record.getBeginTime()!)
                     case .plannedTask(let plannedTask):
-                        Text("")
-                            .frame(height: 80)
+                        PlannedTaskCard(plannedTask: plannedTask)
                             .timelineCardify(
                                 color: timeline.taskCategory(id: plannedTask.taskCategoryId).color,
                                 time: record.getBeginTime()!)
@@ -197,6 +196,94 @@ struct CompletedTaskCard: View {
     }
 }
 
+struct PlannedTaskCard: View {
+    @EnvironmentObject var timeline: Timeline
+    var plannedTask: PlannedTask
+    
+    var body: some View {
+        HStack {
+            VStack {
+                HStack {
+                    timeline.taskCategory(id: plannedTask.taskCategoryId).icon
+                        .foregroundColor(Color(red: 1, green: 0.85, blue: 0.35))
+                    Text(timeline.taskCategory(id: plannedTask.taskCategoryId).name)
+                        .foregroundColor(.gray)
+                }
+                .font(.footnote)
+                Spacer()
+                ZStack {
+                    RoundedRectangle(cornerRadius: TimelineCardify.CardConstants.cornerRadius)
+                            .foregroundColor(timeline.taskCategory(id: plannedTask.taskCategoryId).color)
+                            .opacity(0.4)
+                    HStack {
+                        Image(systemName: "calendar")
+                            .foregroundColor(.white)
+                        Text(plannedTask.attachedRepeatPlanId == nil ? "计划任务" : "重复计划")
+                            .fontWeight(.black)
+                            .foregroundColor(.white)
+                    }
+                    .font(.system(size: 10))
+                }
+            }
+            .frame(minWidth: 80, maxWidth: 80)
+            Spacer()
+            
+            VStack (alignment: .leading) {
+                Text("\(getTimeString(of: plannedTask.beginTime)) - \(getTimeString(of: plannedTask.endTime))")
+                    .font(.headline)
+                    .padding(.vertical, 2)
+                
+                HStack{
+                    Image(systemName: "list.dash.header.rectangle")
+                    Text(plannedTask.taskDescription)
+                }
+                .padding(.leading, 10)
+                .font(.footnote)
+                .foregroundColor(.gray)
+                
+                HStack{
+                    Image(systemName: "clock")
+                    VStack(alignment: .leading) {
+                        Text("计划时长：\(timeStringFromSeconds(plannedTask.durationInSeconds))")
+                        
+                        if plannedTask.isOver {
+                            Text("实际时长：\(timeStringFromSeconds(plannedTask.totalExecutionTimeInSeconds))")
+                        } else {
+                            Text("实际时长：尚未执行")
+                        }
+                    }
+                }
+                .padding(.leading, 12)
+                .padding(.vertical, 1)
+                .font(.footnote)
+                .foregroundColor(.gray)
+                
+                HStack {
+                    Image(systemName: "rosette")
+                        .foregroundColor(.gray)
+                    if plannedTask.isOver {
+                        Text("已完成 ✓")
+                            .foregroundColor(.green)
+                    } else {
+                        Text("未完成 ✘")
+                            .foregroundColor(.red)
+                    }
+                }
+                .padding(.leading, 12)
+                .font(.footnote)
+                
+            }
+            
+            Spacer()
+            RoundedRectangle(cornerRadius: 2)
+                .frame(width: 4)
+                .opacity(0.4)
+                .foregroundColor(timeline.taskCategory(id: plannedTask.taskCategoryId).color)
+                .padding(8)
+        }
+        .padding()
+    }
+}
 
 
 
